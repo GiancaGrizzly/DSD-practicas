27e20d936eccb382bd07e281d365b7342cabdf02
GiancaGrizzly <carloscorteza5@gmail.com>
2022-04-14 21:07:16 +0200

    feat: interfaz entre servidores

--- practica3-2/IServidor.java
+++ practica3-2/IServidor.java
@@ -1,8 +1,13 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
 // Define la interfaz entre las réplicas
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
-
-public interface IServidor
+public interface IServidor extends Remote
 {
-    
+    int GetTotalSuscriptores() throws RemoteException;
+    double GetSubtotalDonado() throws RemoteException;
 }
