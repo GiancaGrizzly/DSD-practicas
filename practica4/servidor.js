41c9d943534d6a7d914a401f159e8c790a28868e
GiancaGrizzly <carloscorteza5@gmail.com>
2022-05-17 20:02:55 +0200

    practica 4 - cliente index.html

---
+++ practica4/servidor.js
@@ -0,0 +1,61 @@
+/* ------------------------------------------------------------------------------
+ * includes
+**/
+
+var http = require("http");
+var express = require("express");
+var socketio = require("socket.io");
+var MongoClient = require('mongodb').MongoClient;
+
+/* ------------------------------------------------------------------------------
+ * Configuración del servidor
+**/
+
+var app = express();
+var httpServer = http.createServer(app);
+
+// Si la url es http://localhost:8080/, se sirve el archivo index.html
+app.get('/', (request, response) => {
+
+    response.sendFile(__dirname + '/index.html');
+});
+
+// Se habilita el uso de los archivos en /static
+// Me daba error --> No se puede cargar la hoja de estilos index.css porque su tipo
+// MIME, text/plain, no es text/css
+app.use(express.static(__dirname + '/static'));
+
+/* ------------------------------------------------------------------------------
+ * Configuración del cliente para mongodb
+ * Inicialización del servidor
+ * Configuración socket.io
+**/
+
+var urlMongo = "mongodb://localhost:27017/";
+var io = socketio(httpServer);
+var connectionEvent = function(socket) {
+
+    console.log("Gianca, tiene un nuevo usuario!");
+
+    socket.on('chat message', function(msg) {
+
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', function() {
+
+        console.log("Gianca, se te ha escapado una escapatraña!");
+    })
+};
+
+MongoClient.connect(urlMongo, {useUnifiedTopology: true}, function(err, db) {
+
+    // El servidor comienza a escuchar en el puerto 8080
+    httpServer.listen(8080, () => {
+
+        console.log("Servidor Domótica iniciado ...");
+    });
+
+    io.on('connection', connectionEvent);
+    
+});
